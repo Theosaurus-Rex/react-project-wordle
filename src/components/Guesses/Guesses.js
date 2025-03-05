727c7f2c42de8e98439db6fb45ac698f57be9ac8
Theosaurus-Rex <theosaurus13@gmail.com>
2025-03-05 11:43:57 +1100

    feat: Render game board with grid for guesses

--- src/components/Guesses/Guesses.js
+++ src/components/Guesses/Guesses.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Guess from "../Guess/Guess";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { range } from "../../utils";
 
 function Guesses({ guesses }) {
   return (
     <div>
-      {guesses.map(({ id, guess }) => (
-        <p key={id}>{guess}</p>
+      {range(NUM_OF_GUESSES_ALLOWED).map(({ num }) => (
+        <Guess key={num} value={guesses[num]} />
       ))}
     </div>
   );
